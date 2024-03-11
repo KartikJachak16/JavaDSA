7582ea93a42f4748cccdd034d0750c354f255764
KartikJachak16 <109535101+KartikJachak16@users.noreply.github.com>
2024-03-11 13:02:52 +0530

    Day 4

---
+++ Hackerrank_Problem.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Hackerrank_Problem {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = 4 ;
+        double d = 4.0;
+        String s1 = "HackerRank ";
+
+        int a1 = sc.nextInt();
+        double d1 = sc.nextDouble();
+        sc.nextLine();
+        String s2 = sc.nextLine();
+        
+
+        System.out.println(a+a1);
+        System.out.println(d+d1);
+        System.out.println(s1+s2);
+
+    }
+}
